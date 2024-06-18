cb488f118bcbd4d2ebc229c00afbd97a91aa7cdc
kumait mustafa <kumait@gmail.com>
2024-06-18 14:38:38 +0200

    update

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import connectDB from "../src/db/connectDB.js";
 import rootRouter from "../src/routes/root.js";
 
 const app = express();
 connectDB();
+
+app.use(
+  cors({
+    origin: process.env.BASE_CLIENT_URL,
+  }),
+);
 
 app.use("/", rootRouter);
 
