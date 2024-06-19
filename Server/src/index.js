3dd14fe3d7cfbaae2f93139a018f2d116d536d9b
kumait mustafa <kumait@gmail.com>
2024-06-19 16:49:08 +0200

    update

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,26 +1,18 @@
 import dotenv from "dotenv";
+import app from "./app.js";
+import { logInfo, logError } from "./util/logging.js";
 import connectDB from "./db/connectDB.js";
+
 dotenv.config();
+const port = process.env.PORT || 5000;
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => logInfo(`Server started on port ${port}`));
+  } catch (error) {
+    logError(error);
+  }
+};
 
-// import dotenv from "dotenv";
-// import app from "./app.js";
-// import { logInfo, logError } from "./util/logging.js";
-// import connectDB from "./db/connectDB.js";
-//
-// dotenv.config();
-// const port = process.env.PORT || 5000;
-//
-// const startServer = async () => {
-//   try {
-//     await connectDB();
-//     app.listen(port, () => logInfo(`Server started on port ${port}`));
-//   } catch (error) {
-//     logError(error);
-//   }
-// };
-//
-// startServer();
-//
-// export default app;
+startServer();
